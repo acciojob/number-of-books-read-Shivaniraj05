1eb146732fd2840863c3ed1d6d73574ef3db3305
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2023-08-22 17:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
-
-alert(numberOfBooksRead());
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
+  
+  const numberOfBooksRead = () => {
+    // write your code here
+      let count=0;
+      for(const x of library){
+        if(x.readingStatus==true){
+            count++;
+        }
+      }
+      return count;
+  };
+  
+  // Do not change the code below
+  
+  alert(numberOfBooksRead());
